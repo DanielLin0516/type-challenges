bdeb35de2025ea9fdc2dea81fc3a9f1a78633967
masx200 <34191203+masx200@users.noreply.github.com>
2022-04-11 02:37:37 +0000

    feat(question): add #8536 - HighDimensionalMapRaw

---
+++ questions/8536-extreme-highdimensionalmapraw/test-cases.ts
@@ -0,0 +1,6 @@
+import { Equal, Expect, ExpectFalse, NotEqual,ExpectExtends } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<HighDimensionalMapRaw<number,number, 4> , Map<number,Map<number,Map<number,Map<number,number>>>>>>,
+   Expect<Equal< Map<number,Map<number,Map<number,Map<number,number>>>>,HighDimensionalMapRaw<number,number, 4> >>,
+]
